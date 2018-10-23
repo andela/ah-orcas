a1ba65eeda69a1ddcee48ab4ac8d0b75d061b275
Gidraf Orenja Mtange <g-draflab@Gidrafs-MacBook-Pro.local>
2018-10-23 17:56:07 +0300

    [finishes #161254652] Configure Authors Haven to use PostgreSQL as DB Engine

--- authors/settings.py
+++ authors/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DATABASE'],
+        'USER':os.environ['USER'],
+        'HOST':os.environ['HOST'],
+        'PASSWORD':os.environ['PASSWORD']
     }
 }
 
