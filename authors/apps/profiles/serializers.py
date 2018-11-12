0a083c9d3ce893375af2247c2ff899d2d7895409
TheSteelGuy <collotuos0@gmail.com>
2018-11-12 09:20:52 +0300

    feat(follow profile): add follow and unfollow another user profile
    - authenticated user should be able to follow another user
    - authenticated user should be able to un-follow another user
    - a user should be able to follow themselves
    - create tests for following
    - fix initial artcle and artcles mix up
    - refactor code
    - add makemigrations for profiles to fix tests
    - fix merge conflict
    - change response to 204 no content
    
    [Delivers #161254668]

--- authors/apps/profiles/serializers.py
+++ authors/apps/profiles/serializers.py
@@ -6,10 +6,11 @@ class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username')
     bio = serializers.CharField(allow_blank=True, required=False)
     image = serializers.SerializerMethodField()
+    following = serializers.SerializerMethodField()
 
     class Meta:
         model = UserProfile
-        fields = ('username', 'bio', 'image',)
+        fields = ('username', 'bio', 'image', 'following',)
         read_only_fields = ('username',)
 
     def get_image(self, obj):
@@ -17,6 +18,18 @@ class ProfileSerializer(serializers.ModelSerializer):
             return obj.image
 
         return 'null'
+
+    def get_following(self, instance):
+        """get the new profile instance which we want to follow"""
+        # get the request context which contains the user
+        request = self.context.get('request', None)
+        if request is None:
+            return False
+        if request.user.is_authenticated:
+            # obtain the profile instance from the request
+            follower = request.user.userprofile
+            followee = instance
+            return follower.is_following(followee)
 
 
 class ProfileListSerializer(serializers.ModelSerializer):
--- release-tasks.sh
+++ release-tasks.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 python manage.py makemigrations authentication
 python manage.py migrate authentication
+python managy.py makemigrations article
+python manage.py migrate article
+python manage.py makemigrations profiles
+python manage.py migrate profiles
 python manage.py makemigrations
 python manage.py migrate
